bb67fce0f8d7881f077c0a17e497881d578f81db
othi <mnpq.raven@gmail.com>
2022-06-09 21:08:46 +0700

    fix router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/mnpqraven.github.io/'
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt-workshop',
