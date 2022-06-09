604be60434d81cca3cb5683df48c76fd42f365e3
Othi <mnpq.raven@gmail.com>
2022-06-10 00:43:01 +0700

    fix indenting, added flex

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,25 @@
 <template>
-<main>
-  <h1>Hello world!</h1>
-  <NuxtLink to="/">Home page</NuxtLink><br>
-  <NuxtLink to="/about">About</NuxtLink><br>
-  <NuxtLink to="/blog/my-first-blog-post">Blog</NuxtLink>
-  <a href="https://nuxtjs.org">normal href link</a>
-  <h1 class="bg-green-700 text-blue-200 font-mono text-4xl">Hello world!</h1>
-  <button @click="count++" class="bg-red-300">count is {{count}} </button>
-  </main>
+    <main>
+        <NuxtLink to="/">Home page</NuxtLink><br>
+        <NuxtLink to="/about">About</NuxtLink><br>
+        <NuxtLink to="/blog/my-first-blog-post">Blog</NuxtLink>
+        <a href="https://nuxtjs.org">normal href link</a>
+        <div class="flex flex-col items-center justify-center">
+            <h1 class="bg-green-700 text-blue-200 font-mono text-4xl">Hello world!</h1>
+            <button @click="count++" class="border-red-900 rounded-md border bg-red-300">count is {{count}} </button>
+        </div>
+    </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
-  data() {
-  return {
-  count:0
-  }
-  }
+    name: 'IndexPage',
+    data() {
+        return {
+            count:0
+        }
+    }
 })
 </script>
