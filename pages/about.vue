b9784a51fc1c19debe39e337a9a58f05c5efa9b7
othi <mnpq.raven@gmail.com>
2022-06-14 22:44:27 +0700

    baseline for navbar, making tailwind works on vscode, about using content

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,18 @@
 <template>
-<main>
-<h1>this is the about page</h1>
-<NuxtLink to="/">Home page</NuxtLink><br>
-</main>
+  <main>
+    <NavBar></NavBar>
+    <div class="ml-16">
+      <nuxt-content :document="about"></nuxt-content>
+    </div>
+  </main>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'AboutPage',
+<script>
+export default ({
+  async asyncData({ $content }) {
+    const about = await $content('about').fetch()
+    //curly
+    return { about }
+  }
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,47 @@
 <template>
-<main>
-  <h1>Hello world!</h1>
-  <NuxtLink to="/">Home page</NuxtLink><br>
-  <NuxtLink to="/about">About</NuxtLink><br>
-  <NuxtLink to="/blog/my-first-blog-post">Blog</NuxtLink>
-  <a href="https://nuxtjs.org">normal href link</a>
-  <h1 class="bg-green-700 text-blue-200 font-mono text-4xl">Hello world!</h1>
-  <button @click="count++" class="bg-red-300">count is {{count}} </button>
+  <main>
+    <NavBar></NavBar>
+    <div class="ml-16">
+      <h1 class="bg-green-700 text-blue-200 font-mono text-4xl">Othi DB!</h1>
+      <p>{inserts content here}</p>
+      <div class="test">
+        Get CSS working <span class="test_red">faster and smarter</span> with <span class="test_working">PostCSS</span>
+      </div>
+      <div>
+        <button @click="count++" class="bg-red-300">count is {{ count }} </button>
+        <button @click="count = 0">reset count</button>
+      </div>
+    </div>
+
   </main>
 </template>
 
+<style lang="postcss">
+.test {
+  color: rgb(40, 176, 218);
+  @apply p-4;
+
+  &_working {
+    @apply text-green-500;
+    @apply font-bold;
+  }
+
+  &_red {
+    @apply text-red-600;
+    @apply font-mono; 
+    @apply font-bold;
+  }
+}
+</style>
 <script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
   data() {
-  return {
-  count:0
-  }
+    return {
+      count: 0
+    }
   }
 })
 </script>
