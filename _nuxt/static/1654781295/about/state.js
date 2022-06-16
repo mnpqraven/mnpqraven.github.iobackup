7fe70f2d4d8341ff585e561ceb81d7951d00b1d6
othi <mnpq.raven@gmail.com>
2022-06-16 20:32:34 +0700

    3a28036

---
+++ _nuxt/static/1654781295/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmnpqraven.github.io\u002F_nuxt\u002Fstatic\u002F1654781295",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fmnpqraven.github.io\u002F",assetsPath:"\u002Fmnpqraven.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"89c30819"}}}}(null));
